e2362633ba7d677bbfd77e2c50f73231792b6d33
uit2712 <phamquangvi456@gmail.com>
2024-09-11 22:07:45 +0000

    add migrations

---
+++ Migrations/20240911220734_Init.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EnglishAspDotNetMvc.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "topics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    GroupId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_topics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_topics_groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vocabularies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Pronunciation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Meaning = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    TopicId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vocabularies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_vocabularies_topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "topics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_topics_GroupId",
+                table: "topics",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vocabularies_TopicId",
+                table: "vocabularies",
+                column: "TopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vocabularies");
+
+            migrationBuilder.DropTable(
+                name: "topics");
+
+            migrationBuilder.DropTable(
+                name: "groups");
+        }
+    }
+}
